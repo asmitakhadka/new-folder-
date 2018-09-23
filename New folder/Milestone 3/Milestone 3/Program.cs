692f6e45521807271c168b659670665d7ddd4f3c
Your Name <you@example.com>
2018-09-23 23:45:20 +1200

    test 2

---
+++ New folder/Milestone 3/Milestone 3/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milestone_3
+{
+    public class client // class for getting the details of customers//
+    {
+        string askdetail;
+        public static void detail()
+        {// void method for asking customer details//
+            Console.WriteLine("We are going to take your details first, Thank you!");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Please enter your name followed by the capital letter:");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Please enter your address:");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Please enter the type of deliver - takeaway or eat here:");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+    public class pizza // class for making the order and payments along with size of pizza//
+    {
+        string Pizzas;
+        int size;
+        public static void aboutpizza()
+        {  // void method for creating a menu to showing a pizzas//
+            Console.WriteLine("These are the Menu of pizza that you have to choose:");
+            List<string> Pizzas = new List<string>();
+            Pizzas.Add("**-----Capricciosa Pizza--------**");
+            Pizzas.Add("**--------Pugliese Pizza------**");
+            Pizzas.Add("**-------Quatro Stagioni Pizza--------**");
+            Pizzas.Add("**-----Pizzetta Pizza-------**");
+            Pizzas.Add("**---------Sicilian pizza------**");
+
+            foreach (string Pizza in Pizzas)
+            {
+                Console.WriteLine(Pizza);
+            }
+            Console.WriteLine("Please enter your pizza type:"); //asking a pizza type to customer//
+            Console.ReadLine();
+            Console.Clear(); //clear a console interface and pass it on next console//
+        }
+        public static void asksize()
+        {//method for asking a size of pizza in inches//
+            int size;
+            Console.WriteLine("Please select the size of pizza: Sizes in between 1 inch and 30 inches:");
+            size = int.Parse(Console.ReadLine());
+
+            if (size <= 10)
+            {
+                Console.WriteLine("Only on $95 for Regular");
+            }
+            else if (size <= 20)
+            {
+                Console.WriteLine("Only on $150 for Medium");
+            }
+            else if (size < 30)
+            {
+                Console.WriteLine("Only on $310 for Large");
+            }
+            string pizza;
+            Console.WriteLine($"Thank you for choosing the {size} inches size of pizza.");//greeting for choosing a size of pizza//
+
+            Console.WriteLine("Now you could select another pizza:");
+
+            Console.WriteLine("Do you want to make an order for any drink; Please press Enter and see the list of drinks with prices:"); //asking for drinks and show the prices of that//
+            Console.ReadLine();
+
+            var drinks = new List<Tuple<string, int>>();// creating list with tuples//
+            drinks.Add(Tuple.Create("**---Cocacola---**", 2));
+            drinks.Add(Tuple.Create("**---Fanta---**", 2));
+            drinks.Add(Tuple.Create("**---Sprite---**", 3));
+            drinks.Add(Tuple.Create("**---Coke Zero---**", 2));
+            drinks.Add(Tuple.Create("**---7 UP---**", 2));
+            drinks.Add(Tuple.Create("**---L & P---**", 2));
+
+            foreach (var drink in drinks)
+            {
+                Console.WriteLine(drink);
+            }
+        }
+        public static void payment()
+        {//asking for confirmed of customers details//
+            int payment;
+            Console.WriteLine("Have you entered your complete details above? Please enter 1 for Yes and 0 for No.");
+            payment = int.Parse(Console.ReadLine());
+            if (payment == 1)
+            {
+                Console.WriteLine("The details have been confirmed - Now you can make a payment for you pizza:");
+            }
+            else if (payment == 0)
+            {
+                Console.WriteLine("Sorry! still need some more details for your order:");
+            }
+            int pay;// once details is confirmed; asking a customer for making a payment of pizza//
+            Console.WriteLine("Proceeded for payment of your order - You have to pay here:");
+            pay = int.Parse(Console.ReadLine());
+
+            if (pay <= 100)
+            {
+                Console.WriteLine($"Wait!! take a change of ${pay - 95} for regular size of pizza:");
+            }
+            else if (pay <= 160)
+            {
+                Console.WriteLine($"Wait!! take a change of ${pay - 150} for medium size of pizza:");
+            }
+            else if (pay <= 320)
+            {
+                Console.WriteLine($"Wait!! take a change of ${pay - 310} for large size of pizza:");
+            }
+            Console.WriteLine("THANK YOU FOR VISIT US:");// greeting message after finish of payments//
+
+        }
+        public static void Main(string[] args) //Main methods with arguments//
+        {//recalling the void methods//
+           client.detail();
+            aboutpizza();
+            asksize();
+            payment();
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ New folder/prog-milestone2-team16/prog-milestone2-team16/Program.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog_milestone2_team16
+{
+    class Program
+    {
+        public static void Main(string[] args) //recalling methods//
+        {
+            asklevel();
+            askid();
+            asksubjectcode();
+            askpaper();
+            askmarks();
+            int result = (60 + 70 + 85 / 3);//average of three supposed marks//
+            Console.WriteLine($"This is an average marks of your papers {60 + 70 + 85 / 3} and you have passed this course:");
+            showgrade();
+            Console.ReadLine();
+        }
+        public static void asklevel()
+        {//method for asking a level of user//
+            int level;
+            Console.WriteLine("Please enter your level:");
+            level = int.Parse(Console.ReadLine());
+
+            if (level == 5)
+            {
+                Console.WriteLine("Perhaps you need 4 papers");
+            }
+            else
+            {
+                Console.WriteLine("Perhaps you need 3 papers");
+            }
+
+            Console.WriteLine($"You are in level {level}:");
+        }
+        public static void askid()
+        {//method for asking an id of user//
+            int id;
+            Console.WriteLine("Please enter your idnumber:");
+            id = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Your id is {id}:");
+
+        }
+        public static void asksubjectcode()
+        {//method for asking paper code of user//
+            int code;
+            Console.WriteLine("Please enter your subject code:");
+            code = int.Parse(Console.ReadLine());
+            
+            if (code >= 500 )
+            {
+                Console.WriteLine("Congratulation! You have got A+ grade");
+            }
+            else if (code <= 600)
+                    {
+                Console.WriteLine("Congratulation! You have got A- grade");
+            }
+            else if (code >= 300)
+            {
+                Console.WriteLine("Congratulation! You are in rank of B+ grade");
+            }
+            else if (code <= 200)
+            {
+                Console.WriteLine("Congratulation! You have got B- grade");
+            }
+            else if (code < 700)
+            {
+                Console.WriteLine("Congratulation! You are in rank of C grade");
+            }
+            Console.WriteLine("Please press enter for further information:");
+            Console.ReadLine();
+        }
+        public static void askpaper ()
+        {//method for showing all papers of level 5 and level 6//
+            Console.Clear();
+            Console.WriteLine("The following are the papers for only level 5 and level 6:");
+
+            var papers = new List<string>();
+            papers.Add("Infrastructure");
+            papers.Add("Professional skills");
+            papers.Add("Networking");
+            papers.Add("Software packages");
+            papers.Add("Statistics");
+            papers.Add("Multimedia");
+            papers.Add("Programming");
+            papers.Add("Advanced Programming");
+            papers.Add("Website designing");
+            papers.Add("Business management");
+            papers.Add("Database management");
+
+            foreach (var paper in papers)
+            {
+                Console.WriteLine(paper);
+            }
+        
+        }
+        public static void askmarks()
+        {//method for printing marks of paper that user want incase// 
+            string paper;
+            Console.WriteLine("Please enter your paper name:");
+            Console.ReadLine();
+
+            int papers;
+            Console.WriteLine("Please enter your expected marks:");
+            papers = int.Parse(Console.ReadLine());
+
+            if (papers <= 60)
+            {
+                Console.WriteLine("You have got C grade that means above 50%:");
+            }
+            else if (papers <= 70)
+            {
+                Console.WriteLine("You have got B grade that means above 70%:");
+            }
+            else if (papers <= 100)
+            {
+                Console.WriteLine("You have got A grade that means above 80%:");
+            }
+            Console.WriteLine("Please press enter for other paper's grade and marks:");
+            Console.ReadLine();
+
+            string paperTypes;//printing other paper's grade and marks//
+            Console.WriteLine("Please enter your paper name:");
+            Console.ReadLine();
+
+            int papersTypes;
+            Console.WriteLine("Please enter your expected marks:");
+            papers = int.Parse(Console.ReadLine());
+
+            if (papers <= 60)
+            {
+                Console.WriteLine("You have got C grade that means above 50%:");
+            }
+            else if (papers <= 70)
+            {
+                Console.WriteLine("You have got B grade that means above 70%:");
+            }
+            else if (papers <= 100)
+            {
+                Console.WriteLine("You have got A grade that means above 80%:");
+            }
+            Console.WriteLine("Please press enter for other paper's mark and grade:");
+            Console.ReadLine();
+
+            string paperAgain;//printing other paper's grade and marks//
+            Console.WriteLine("Please enter your paper name:");
+            Console.ReadLine();
+
+            int papersAgain;
+            Console.WriteLine("Please enter your expected marks:");
+            papers = int.Parse(Console.ReadLine());
+            if (papers <= 60)
+            {
+                Console.WriteLine("You have got C grade that means above 50%:");
+            }
+            else if (papers <= 70)
+            {
+                Console.WriteLine("You have got B grade that means above approx.70%:");
+            }
+            else if (papers >= 85)
+            {
+                Console.WriteLine("You have got A grade that means above 85%:");
+            }
+            Console.WriteLine("Please press enter for other information:");
+            Console.ReadLine();
+        }
+        public static int averagemarks(int papers, int papersType, int papersAgain)
+        {//return an integers and giving average marks of user's marks//
+            return papers + papersType + papersAgain / 3;
+        }
+        public static void showgrade()
+        {// method for showing paper that has A+ grade//
+            string grade;
+            Console.WriteLine("Please choose one paper that you want to know about grade:");
+            Console.ReadLine();
+
+            int grades;
+            Console.WriteLine("Please enter number that should be above 85, thanks:");
+            grades = int.Parse(Console.ReadLine());
+
+            if (grades>= 85)
+            {
+                Console.WriteLine($"This paper have A+ grade having {grades} marks , Thank you!");
+            }
+            else
+            {
+                Console.WriteLine("This is not a paper having A+ grade, Thank you!");
+            }
+                } 
+    }
+}
+     
+  
+
+
+
+
